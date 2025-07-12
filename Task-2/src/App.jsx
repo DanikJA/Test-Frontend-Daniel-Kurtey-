c8596d40156a56d91c0d8b88d5832b4adf715983
Danik2023 <danik.kyrteu2@gmail.com>
2025-07-12 15:55:50 +0300

    created modal window and script to count the amount of images with showing the current date and time

--- Task-2/src/App.jsx
+++ Task-2/src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Gallery from "./Gallery";
 import "./Gallery.css";
+import { images } from "./Gallery";
+import "./App.css";
 
 function App() {
+  const [dateNow, setDateNow] = useState("");
+
+  useEffect(() => {
+    const now = new Date();
+    const pad = (n) => String(n).padStart(2, "0");
+
+    const formatted =
+      `${pad(now.getDate())}.${pad(now.getMonth() + 1)}.${now.getFullYear()} ` +
+      `${pad(now.getHours())}:${pad(now.getMinutes())}`;
+
+    setDateNow(formatted);
+  }, []);
+
   return (
     <div>
       <h1>Галерея</h1>
+      <div className="info-bar">
+        Кількість зображень:{images.length} | Станом на: {dateNow}
+      </div>
       <Gallery />
     </div>
   );
--- Task-2/src/Gallery.jsx
+++ Task-2/src/Gallery.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import "./Gallery.css";
+
 import img1 from "./assets/images/1.jpg";
 import img2 from "./assets/images/2.jpg";
 import img3 from "./assets/images/3.jpg";
@@ -10,9 +13,8 @@ import img9 from "./assets/images/9.jpg";
 import img10 from "./assets/images/10.jpg";
 import img11 from "./assets/images/11.jpg";
 import img12 from "./assets/images/12.jpg";
-import "./Gallery.css";
 
-const images = [
+export const images = [
   img1,
   img2,
   img3,
@@ -28,12 +30,31 @@ const images = [
 ];
 
 function Gallery() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   return (
-    <div className="gallery">
-      {images.map((src, index) => (
-        <img key={index} src={src} alt={`Image ${index + 1}`} />
-      ))}
-    </div>
+    <>
+      <div className="gallery">
+        {images.map((src, index) => (
+          <img
+            key={index}
+            src={src}
+            alt={`Image ${index + 1}`}
+            onClick={() => setSelectedImage(src)}
+          />
+        ))}
+      </div>
+
+      {selectedImage && (
+        <>
+          <div className="overlay" onClick={() => setSelectedImage(null)}></div>
+          <div className="modal">
+            <img src={selectedImage} alt="Full-size" />
+            <button onClick={() => setSelectedImage(null)}>✖</button>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
