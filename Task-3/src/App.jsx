a985bc7f1529f6985b03694e13815f3bfe6e5c70
Danik2023 <danik.kyrteu2@gmail.com>
2025-07-14 17:54:32 +0300

    fetching data, showing info about the university

--- Task-3/src/App.jsx
+++ Task-3/src/App.jsx
@@ -1,8 +1,73 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import UniversityTable from "./components/UniversityTable";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [country, setCountry] = useState("");
+  const [universities, setUniversities] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("");
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        `http://universities.hipolabs.com/search?country=${country}`
+      );
+      const data = await res.json();
+      setUniversities(data);
+      setSelectedCountry(country);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const resetData = () => {
+    setCountry("");
+    setUniversities([]);
+    setSelectedCountry("");
+  };
+
+  return (
+    <div className="container">
+      <h1>University Finder</h1>
+      <p>Selected:{selectedCountry}</p>
+      <div className="formInput">
+        <input
+          type="text"
+          placeholder="Введіть країну латиницею( наприклад: united states)"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        ></input>
+        <button className="SendBtn" onClick={fetchData}>
+          Send
+        </button>
+        <button className="SendBtn" onClick={resetData}>
+          Reset
+        </button>
+      </div>
+
+      <div className="list">
+        {universities.length > 0 ? (
+          <ul>
+            {universities.map((university, index) => (
+              <li key={index}>
+                <strong>{university.name}</strong>
+                <br />
+                <a
+                  href={university.web_pages[0]}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Перейти
+                </a>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>Немає результатів</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
